3190b2a3cf0427b46474ea8333cbb9be756fe575
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-08-22 14:23:30 +0500

    Events Mouse and Fields

---
+++ 48_Events Fields/script.js
@@ -0,0 +1,7 @@
+function makeFieldYellow(){
+    document.querySelector('input[type="text"]').style.backgroundColor='yellow';
+}
+
+function makeFieldWhite(){
+    document.querySelector('input[type="text"]').style.backgroundColor='white';
+}

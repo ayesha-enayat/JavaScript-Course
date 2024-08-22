b718fb027745d482a6bd6c1f2924f2adf0678591
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-08-22 20:05:40 +0500

    swaping images and setting classes

---
+++ 53_Swaping Images and setting classes/script.js
@@ -0,0 +1,17 @@
+ function swapPic() {
+    document.getElementById("before").src = "https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?size=626&ext=jpg&ga=GA1.1.1819120589.1724284800&semt=ais_hybrid";
+ }
+    
+
+ //the function a general-purpose routine that can swap any image for any
+// other image
+ /*1 function swapPic(eId, newPic) {
+2 document.getElementById(eId).src = newPic;
+3 }
+ */
+
+function getAddress(){
+   var link = document.getElementById("before");
+ var address = link.href;
+ }
+

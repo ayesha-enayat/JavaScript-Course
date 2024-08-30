a2fe32366062ded18569a278c48dc2003fdf0395
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-08-30 09:56:47 +0500

    for loop

---
+++ 18_For loop/index.js
@@ -0,0 +1,58 @@
+var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+var cityToCheck="Great Falls";
+
+// for (var i = 0; i <= 4; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//     console.log("It's one of the cleanest cities");
+//     }
+//     }
+    
+
+
+// cityToCheck="Pakistan";
+// var matchFound = "no"; 
+//  for (var i = 0; i <= 4; i++){
+//  if (cityToCheck === cleanestCities[i]) { 
+//      matchFound = "yes"; 
+//      console.log("It's one of the cleanest cities"); 
+
+//  } 
+//}
+ 
+//  if (matchFound === "no") {
+ 
+//     console.log("It's not on the list");
+ 
+// }
+
+
+
+// var matchFound = false;
+// for (var i = 0; i <= 4; i++){
+// if (cityToCheck === cleanestCities[i]) {
+//     matchFound = true;
+//     console.log("It's one of the cleanest cities");
+//     break;
+// }
+// }
+
+// if (matchFound === false) {
+
+//     console.log("It's not on the list");
+
+// }
+
+// var matchFound = false;
+// for (var i = 0; i <cleanestCities.length; i++){
+// if (cityToCheck === cleanestCities[i]) {
+//     matchFound = true;
+//     console.log("It's one of the cleanest cities");
+//     break;
+// }
+// }
+
+// if (matchFound === false) {
+
+//     console.log("It's not on the list");
+
+// }

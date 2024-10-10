2a02f9a833988b87cc0c1304afcd748742bfc150
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-10-10 13:14:33 +0500

    prcatice till chapter 20

--- 06_JS For Loop/index.js
+++ 06_JS For Loop/index.js
@@ -1,9 +1,14 @@
 var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
 var cityToCheck="Great Falls";
 
+
+// for(i=0;i<cleanestCities.length;i++){
+//     console.log(cleanestCities[i]);
+    
+// }
 // for (var i = 0; i <= 4; i++) {
 //     if (cityToCheck === cleanestCities[i]) {
-//     console.log("It's one of the cleanest cities");
+//     console.log("It's one of the cleanest cities",cleanestCities[i]);
 //     }
 //     }
     
@@ -14,16 +19,15 @@ var cityToCheck="Great Falls";
 //  for (var i = 0; i <= 4; i++){
 //  if (cityToCheck === cleanestCities[i]) { 
 //      matchFound = "yes"; 
-//      console.log("It's one of the cleanest cities"); 
-
+//      console.log("pakistan is in the list"); 
 //  } 
-//}
- 
-//  if (matchFound === "no") {
- 
+//  else{
+//     console.log("pakistan isnot in the list");
+//  }
+//
+//  if (matchFound === "no"){
 //     console.log("It's not on the list");
- 
-// }
+//  }
 
 
 
@@ -57,5 +61,25 @@ var cityToCheck="Great Falls";
 
 // }
 
+/*Nested For Loop*/
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+//  fullNames.push(firstNames[i] + lastNames[j]);
 
+//  }
+//  console.log(fullNames);
+ 
+//  }
 
+/*Print patern using nested for loop */
+
+// for(i=0;i<=5;i++){
+//     for(j=0;j<i;j++){
+//         console.log('*')
+     
+//     }
+//     console.log('</br>')
+// }
---
+++ 06_JS For Loop/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+cityToCheck="Pakistan";
+// var matchFound = "no"; 
+//  for (var i = 0; i <= 4; i++){
+//  if (cityToCheck === cleanestCities[i]) { 
+//      matchFound = "yes"; 
+//      console.log("pakistan is in the list"); 
+//  } 
